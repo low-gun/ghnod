ccdd5c0cdbd79948e3d76176ee47ac6491e058d4
low-gun <low-gun@gmail.com>
2025-06-30 17:48:44 +0900

    fix: use process.env for NEXT_PUBLIC_API_BASE_URL in next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
-
+// frontend/next.config.mjs
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    API_BASE_URL: process.env.API_BASE_URL,
+  },
 };
 
 export default nextConfig;
