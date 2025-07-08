12872f40120d916244846202b6a00768131c5ffb
low-gun <low-gun@gmail.com>
2025-07-08 11:18:46 +0900

    원상복구

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,22 @@ const nextConfig = {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     API_BASE_URL: process.env.API_BASE_URL,
   },
+  experimental: {
+    isrMemoryCacheSize: 0, // ISR 캐시 메모리 비활성화
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // 모든 경로에 대해
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate", // 캐시 완전 차단
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
