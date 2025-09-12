4db9fd6a2b9f79571b3a9c218b1ba51512958b6c
low-gun <low-gun@gmail.com>
2025-09-12 11:05:40 +0900

    dd

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: "https://orpconsulting.co.kr",
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: "daily",
+    priority: 0.7,
+  };
+  
+  export default config;
+  
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,11 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#ffffff" />
-        {/* viewport meta는 _app.js / Head에서 처리 */}
-      </Head>
+ <Head>
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="theme-color" content="#ffffff" />
+
+  {/* SEO 기본 메타 */}
+  <meta name="description" content="GHNOD - 교육, 컨설팅, FTShop을 제공하는 전문 플랫폼" />
+  <meta name="keywords" content="교육, 컨설팅, FTShop, GHNOD" />
+  <meta name="author" content="주식회사 오알피연구소" />
+
+  {/* Open Graph */}
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="GHNOD" />
+  <meta property="og:description" content="교육, 컨설팅, FTShop을 제공하는 전문 플랫폼" />
+  <meta property="og:image" content="/og-image.png" />
+  <meta property="og:url" content="https://orpconsulting.co.kr" />
+</Head>
+
       <body>
         <Main />
         <NextScript />
