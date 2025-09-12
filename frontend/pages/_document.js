9bd85468e017f266338505a73d1bad620890c2b8
low-gun <low-gun@gmail.com>
2025-09-12 14:33:21 +0900

    dd

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
   <link rel="icon" href="/favicon.ico" />
   <meta name="theme-color" content="#ffffff" />
 
+  {/* 네이버 소유권 확인 */}
+  <meta name="naver-site-verification" content="af88184daaa76d1b3f39f3811a604129cffc" />
+
   {/* SEO 기본 메타 */}
   <meta name="description" content="ORP컨설팅 - 진단, 교육, 컨설팅, 조직개발, Hogan, 숙의토론, 공개교육, 팀빌딩" />
   <meta name="keywords" content="ORP컨설팅, 교육, 컨설팅, 공개교육, 팀빌딩, 숙의토론, 공론화, Hogan, Hogan진단" />
@@ -19,6 +22,7 @@ export default function Document() {
   <meta property="og:image" content="/logo_blue.png" />
   <meta property="og:url" content="https://orpconsulting.co.kr" />
 </Head>
+
 
       <body>
         <Main />
